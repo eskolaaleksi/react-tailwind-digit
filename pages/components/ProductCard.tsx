4cd674524445b2356f6fa57e048f330bb82ba73a
eskolaaleksi <32136715+eskolaaleksi@users.noreply.github.com>
2022-11-01 15:17:14 +0200

    Rounded picture borders in dark mode

--- pages/components/ProductCard.tsx
+++ pages/components/ProductCard.tsx
@@ -4,7 +4,7 @@ function Product({
   return (
     <div className="flex flex-wrap w-full p-2 sm:w-1/2 md:w-1/3 xl:w-1/4">
       <div className="flex bg-white w-full rounded-lg dark:bg-gray-300 hover:shadow-2xl transition-shadow duration-300">
-        <div className="w-1/3 overflow-hidden dark:bg-white">
+        <div className="w-1/3 overflow-hidden rounded-lg dark:bg-white">
           <div
             style={{ backgroundImage: `url(${image})` }}
             className="bg-contain bg-no-repeat bg-center w-full h-full duration-300 hover:transform hover:scale-125"
