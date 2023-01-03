9000feab8a6d217dd29c6a117e655486d7b43437
eskolaaleksi <32136715+eskolaaleksi@users.noreply.github.com>
2023-01-03 14:06:54 +0200

    Cleaned code and added comments

--- pages/components/ProductCard.tsx
+++ pages/components/ProductCard.tsx
@@ -2,19 +2,32 @@ function Product({
   product: { title, price, description, category, image },
 }: any) {
   return (
-    <div className="flex flex-wrap w-full p-2 sm:w-1/2 md:w-1/3 xl:w-1/4">
-      <div className="flex bg-white w-full rounded-lg dark:bg-gray-300 hover:shadow-2xl transition-shadow duration-300">
-        <div className="w-1/3 overflow-hidden rounded-lg dark:bg-white">
+    /* flex: täyttää saatavilla olevan tilan, p-2: lisää tyhjää tilaa reunoille
+      w-full: oletusleveys 100%, loput määrittelee leveyden näytön koosta riippuvaiseksi (skaalautuminen ikkunan leveyteen) */
+    <div className="flex p-2 w-full sm:w-1/2 md:w-1/3 xl:w-1/4">
+      {/* bg-white: taustaväri valkoinen, rounded-lg: pyöristetyt kulmat, dark: dark mode asetukset, hover-2xl: elementin hover tilalle varjo */}
+      <div className="flex w-full bg-white rounded-lg dark:bg-gray-300 hover:shadow-2xl">
+        {/* overflow-hidden: piilotetaan elementin kuvan osat, mitkä ylittää elementin rajat */}
+        <div className="w-1/3 p-2 overflow-hidden rounded-lg dark:bg-white">
+          {/* KUVA */}
           <div
+            // lisätään taustakuvaksi tuotteen kuva
             style={{ backgroundImage: `url(${image})` }}
-            className="bg-contain bg-no-repeat bg-center w-full h-full duration-300 hover:transform hover:scale-125"
+            // bg-contain: taustakuvan skaalaus ilman rajausta tai venytystä, bg-no-repeat: näytä taustakuva vain kerran, äläkä täytä aluetta kuvilla
+            // hover tilalle zoomaus ja kestoksi 300ms
+            className="bg-contain bg-no-repeat bg-center w-full h-full hover:transform hover:scale-150 duration-300"
           ></div>
         </div>
-        <div className="w-2/3 p-5 overflow-hidden">
-          <h1 className="text-2xl">{title.substr(0, 50)}</h1>
+        {/* TEKSTI */}
+        <div className="w-2/3 p-1 ">
+          {/* otsikolle iso teksti ja ensimmäiset 40 merkkiä */}
+          <h1 className="text-2xl">{title.substr(0, 40)}</h1>
+          {/* kategorialle eri värinen teksti */}
           <h3 className="text-blue dark:text-gray-700">{category}</h3>
-          <p className="text-blue-shock mt-4 text-xl md:text-4xl">{price}$</p>
-          <div className="mt-4">{description.substr(0, 100)}</div>
+          {/* my-2: marginaali y-akselille */}
+          <p className="text-blue-shock my-2 text-2xl">{price}$</p>
+          {/* kuvauksen ensimmäiset 100 merkkiä */}
+          <div>{description.substr(0, 100)}</div>
         </div>
       </div>
     </div>
